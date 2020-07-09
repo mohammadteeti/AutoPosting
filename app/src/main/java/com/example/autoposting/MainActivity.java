5205e9b4316ef9620e26ac391cbc6246db22cca8
mohammadteeti <37085987+mohammadteeti@users.noreply.github.com>
2020-07-09 14:18:04 +0300

    new update

--- app/src/main/java/com/example/autoposting/MainActivity.java
+++ app/src/main/java/com/example/autoposting/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.autoposting;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView info ;
+    private LoginButton login ;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        info =findViewById(R.id.info);
+        login=findViewById(R.id.login_button);
+
+        callbackManager= CallbackManager.Factory.create();
+
+        login.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User ID  = " + loginResult.getAccessToken().getUserId());
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+
     }
 }
